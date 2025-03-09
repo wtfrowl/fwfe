ab062272ed12c698d6405c6a7131e6993fb5a20c
wtfrowl <imrhjat@rediffmail.com>
2025-03-09 06:54:39 +0530

    xf

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
       <Router>
       <Routes>
         <Route path="/" element={<WebHome/>} />
+        <Route path='/fwfe' element={<WebHome/>}/>
         <Route path="/owner-login" element={<Ologin/>} />
         <Route path="/owner-home" element={<OwnerHome />}>
             <Route index element={<OwnerDashboard />} />
